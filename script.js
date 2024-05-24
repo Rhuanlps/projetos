22bc313d2a24de1fae52e066f564e5419b748eaf
Rhuanlps <165963052+Rhuanlps@users.noreply.github.com>
2024-05-23 22:26:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const baseImagens = {
-        todas: ['imagem.jpg', 'cidade.jpg', 'animal.jpg', 'natureza.jpg'],
-        natureza: [],
-        cidade: [],
-        animais: [],
+        todas: ['imagem.jpg', 'cidade.jpg', 'animais.jpg', 'natureza.jpg'],
+        natureza: ['natureza.jpg'],
+        cidade: ['cidade.jpg'],
+        animais: ['animais.jpg'],
     }
 
+    function carregaImagens(categoria){
+        //selecionar elementos HTML com base em um seletor
+        const galeria = document.querySelector("#galeria-imagens");
+        galeria.innerHTML = ''; //Limpar bloco, paga tudo dentro
+        const imagens = baseImagens[categoria];
+
+        imagens.forEach(img => {
+            galeria.innerHTML += '<div> <img src="imagens/'+img +'" />  </div>';
+        })
+
+    }
+
+    carregaImagens('todas');
+
+    //Evento de clique
     document.body.addEventListener('click', function (event) {
 
         if (event.target.classList.contains('botao-categoria')){
-            alert(123)
+            
+            const categoria = event.target.dataset.categoria;
+
+            carregaImagens(categoria);
         }
 
     })
