9b9b91be8b3ce294e7539f8f9a87a7ffd22ad819
Rhuanlps <165963052+Rhuanlps@users.noreply.github.com>
2024-05-29 20:35:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const baseImagens = {
-        todas: ['imagem.jpg', 'cidade.jpg', 'animais.jpg', 'natureza.jpg'],
+        todas: ['cidade.jpg', 'animais.jpg', 'natureza.jpg'],
         natureza: ['natureza.jpg'],
         cidade: ['cidade.jpg'],
         animais: ['animais.jpg'],
@@ -10,26 +10,39 @@ document.addEventListener('DOMContentLoaded', function(){
     function carregaImagens(categoria){
         //selecionar elementos HTML com base em um seletor
         const galeria = document.querySelector("#galeria-imagens");
-        galeria.innerHTML = ''; //Limpar bloco, paga tudo dentro
+        galeria.innerHTML = ' '; //Limpar bloco, paga tudo dentro
         const imagens = baseImagens[categoria];
 
         imagens.forEach(img => {
-            galeria.innerHTML += '<div> <img src="imagens/'+img +'" />  </div>';
+            galeria.innerHTML += '<div class="imagem-item" > <img src="imagens/'+img +'" />  </div>';
+        })
+
+    }
+    
+    function ordenaImagem(ordem){
+        
+        const imagem = Array.from(document.querySelectorAll("#galeria-imagens .imagem-item"));
+        
+        imagens.sort((a, b) => {
+            console.log(a);
         })
 
     }
 
-    carregaImagens('todas');
-
     //Evento de clique
     document.body.addEventListener('click', function (event) {
 
-        if (event.target.classList.contains('botao-categoria')){
-            
+        if (event.target.classList.contains('botao-categoria')){            
             const categoria = event.target.dataset.categoria;
-
             carregaImagens(categoria);
         }
+
+        if(event.target.classList.contains('botao-ordenar')){
+            const ordem = event.target.dataset.ordem;
+            ordenaImagem(ordem);
+        }
+
+        carregaImagens('todas');
 
     })
 
